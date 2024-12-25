5a7069d8cd9c333b6cb0b53fa8f00f61b4db62bd
chinpeerapat <88846925+chinpeerapat@users.noreply.github.com>
2024-12-25 16:09:45 +0700

    Initialize new-chat-btn

---
+++ config/index.ts
@@ -0,0 +1,24 @@
+import type { AppInfo } from '@/types/app'
+export const APP_ID = `${process.env.APP_ID}`
+export const API_KEY = `${process.env.APP_KEY}`
+export const API_URL = `${process.env.API_URL}`
+export const SA_API_URL = `${process.env.SA_API_URL || 'https://speedyagency.demo2.mixmedia.com/api'}`
+export const SA_API_TOKEN = `${process.env.SA_API_TOKEN}`
+export const ENABLE_AUTH = `${process.env.ENABLE_AUTH}`
+export const SECRET_KEY = `${process.env.SECRET_KEY || 'IEboqt3vmYau9ic6zye78sX2l4JfVHFw'}`
+export const APP_INFO: AppInfo = {
+  title: 'ZoEasy',
+  description: '',
+  copyright: '',
+  privacy_policy: '',
+  default_language: 'zh-Hant',
+}
+
+export const isShowPrompt = false
+export const promptTemplate = 'I want you to act as a javascript console.'
+
+export const API_PREFIX = '/api'
+
+export const LOCALE_COOKIE_NAME = 'locale'
+
+export const DEFAULT_VALUE_MAX_LEN = 48
